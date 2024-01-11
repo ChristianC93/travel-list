a5fa7a93eb5326805da582f0f3134a45cdd564f9
Christian Castillo <castillochristian3@gmail.com>
2024-01-11 11:25:47 -0500

    created Item component to render individual items in PackingList component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+const initialItems = [
+  { id: 1, description: 'Passports', quantity: 2, packed: false },
+  { id: 2, description: 'Socks', quantity: 12, packed: true },
+  { id: 3, description: 'Charger', quantity: 1, packed: false },
+];
+
 export default function App() {
   return (
     <div className="app">
@@ -12,6 +18,7 @@ export default function App() {
 function Logo() {
   return <h1>🏝️ Far Away 🧳</h1>;
 }
+
 function Form() {
   return (
     <div className="add-form">
@@ -19,12 +26,33 @@ function Form() {
     </div>
   );
 }
+
 function PacklingList() {
-  return <div className="list">LIST</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
 }
+
+function Item({ item }) {
+  return (
+    <li key={item.id}>
+      <span style={item.packed ? { textDecoration: 'line-through' } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+
 function Stats() {
   return (
-    <footer>
+    <footer className="stats">
       <em>💼 You have x items on your list, and you already packed x.</em>
     </footer>
   );
